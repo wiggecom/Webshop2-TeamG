e96d70b243e4dd97eb2fc184a42ff624565ef7d7
wiggecom <wigge@hotmail.com>
2023-12-19 07:53:15 +0100

    Initial commit

---
+++ Webshop2-TeamG/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Webshop2_TeamG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
